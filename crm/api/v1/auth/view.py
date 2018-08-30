b1f0a432eee5a65db778fb1a2c6b58d6fb90b655
Alexey <alexey.sergeevich.golubev@gmail.com>
2018-08-31 01:44:16 +0300

    T4 added authenticate

---
+++ crm/api/v1/auth/view.py
@@ -0,0 +1,26 @@
+import datetime
+
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializers import AuthTokenSerializer
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+class ObtainAuthToken(APIView):
+    serializer_class = AuthTokenSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            token, created = Token.objects.get_or_create(user=serializer.validated_data['user'])
+
+            if not created:
+                token.created = datetime.datetime.utcnow()
+                token.save()
+
+            return Response({'token': token.key})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+obtain_auth_token = ObtainAuthToken.as_view()
