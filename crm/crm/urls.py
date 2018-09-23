de0ff813e40a5dda0aa920280ac854216aaaecd6
Alexey <alexey.sergeevich.golubev@gmail.com>
2018-09-23 15:54:48 +0300

    T3 update urls.py

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -10,14 +10,18 @@ from api.v1.storage.routes import storage_router
 from api.v1.temp_token.views import temporary_auth_token
 from api.v1.task.routes import task_router
 from api.v1.personal_account.views import personal_account
-from rest_framework.authtoken import views
 
-from api.v1.auth.view import obtain_auth_token, reset_password
+from api.v1.auth.view import reset_password, confirm_email
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from rest_framework import permissions
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 schema_view = get_schema_view(
@@ -36,17 +40,35 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('account', personal_account, name='account'),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+
     re_path(
         r'^swagger(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(cache_timeout=0), name='schema-json'),
+
     path('swagger/', schema_view.with_ui(
         'swagger', cache_timeout=0), name='schema-swagger-ui'),
+
     path('redoc/', schema_view.with_ui(
         'redoc', cache_timeout=0), name='schema-redoc'),
+
+
+    path('auth/confirm-email/<str:decoded>', confirm_email, name='confirm-email'),
+
+    path('auth/reset-password/', reset_password, name='reset-password'),
+
+
+    path('temp-token/<str:token>/', temporary_auth_token, name='temp_token'),
+
+
     path('admin/', admin.site.urls),
-    path('token/', obtain_auth_token, name='token'),
-    path('reset-password/', reset_password, name='reset-password'),
+
+
+    path('account', personal_account, name='account'),
 
     path('client/', include(
         (client_router.urls, 'client_app'), namespace='client')),
@@ -71,6 +93,4 @@ urlpatterns = [
 
     path('user/', include(
         (user_router.urls, 'user_app'), namespace='user')),
-
-    path('temp-token/<str:token>/', temporary_auth_token, name='temp_token')
 ]
