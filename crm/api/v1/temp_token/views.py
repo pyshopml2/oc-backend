4a1c52a2ebbe81c820e7e04978c81f5165a1abe7
Alexey <alexey.sergeevich.golubev@gmail.com>
2018-09-23 15:51:56 +0300

    T3 updated temporary token view

--- crm/api/v1/temp_token/views.py
+++ crm/api/v1/temp_token/views.py
@@ -1,31 +1,33 @@
 import datetime
 
+from django.contrib.auth import get_user_model
+
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 
-from .serializers import *
 from temp_token.models import TempToken
-from core.tests.consts import DATE_TIME_TZ, TZ
+from core.tests.consts import DATE_TIME_TZ
 from api.v1.user.serializers import UserSerializer
+
 
 User = get_user_model()
 
 
 class TempTokenView(APIView):
-
-    def get(self, request, **kwargs):
+    def get(self, request, *args, **kwargs):
         temp_token = kwargs.get('token')
         try:
-            if self.check(temp_token):
+            if self.check_availability(temp_token):
                 user = User.objects.get(
                     auth_temp_token__token=temp_token)
-                token = user.auth_token
+                refresh_token = RefreshToken.for_user(user)
+                access_token = AccessToken.for_user(user)
                 user_serializer = UserSerializer(user)
                 return Response(
                     user_serializer.data,
-                    headers={'Authorization': 'Token ' + str(token)},
+                    headers={'refresh': refresh_token, 'access': access_token},
                     status=status.HTTP_200_OK)
             else:
                 return Response('Token is not active or time expired')
@@ -36,11 +38,13 @@ class TempTokenView(APIView):
             return Response(data='Token does not exist',
                             status=status.HTTP_400_BAD_REQUEST)
 
-    def check(self, temp_token):
+    def check_availability(self, temp_token):
         temp_token = TempToken.objects.get(token=temp_token)
         if datetime.datetime.now().strftime(DATE_TIME_TZ) < \
                 temp_token.expiration_date.strftime(DATE_TIME_TZ) \
                 and temp_token.active:
+            temp_token.active = False
+            temp_token.save()
             return True
         else:
             return False
